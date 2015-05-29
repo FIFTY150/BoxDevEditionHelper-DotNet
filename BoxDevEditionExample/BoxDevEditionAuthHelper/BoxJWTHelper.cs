e5e142976c8795d2b8d4dc8b68e45aa3c244445d
Chad Burnette <chadburnette@me.com>
2015-05-28 22:39:46 -0400

    initial commit

---
+++ BoxDevEditionExample/BoxDevEditionAuthHelper/BoxJWTHelper.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.IO;
+using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto;
+
+/* Use Nuget to install the following packages:
+ * 
+ * Bouncy Castle
+ * JSON Web Token Handler for the Microsoft .NET Framework 4.5
+ * Json.NET
+ * RestSharp
+ * 
+ * Also, add a reference to System.IdentityModel
+ */
+
+namespace BoxDevEditionAuthHelper
+{
+    public class BoxJWTHelper
+    {
+        const string AUTH_URL = "https://api.box.com/oauth2/token";
+        const string USERS_URL = "https://api.box.com/2.0/users";
+        const string JWT_GRANT_TYPE = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+
+        private readonly string enterpriseId;
+        private readonly string clientId;
+        private readonly string clientSecret;
+        private readonly SigningCredentials credentials;
+
+        public BoxJWTHelper(string enterpriseId, string clientId, string clientSecret, string privateKey, string privateKeyPassword)
+        {
+            this.enterpriseId = enterpriseId;
+            this.clientId = clientId;
+            this.clientSecret = clientSecret;
+
+            var pwf = new PEMPasswordFinder(privateKeyPassword);
+            AsymmetricCipherKeyPair key;
+            using (var reader = new StringReader(privateKey))
+            {
+                key = (AsymmetricCipherKeyPair)new PemReader(reader, pwf).ReadObject();
+            }
+            var rsa = DotNetUtilities.ToRSA((RsaPrivateCrtKeyParameters)key.Private);
+
+            this.credentials = new SigningCredentials(new RsaSecurityKey(rsa), SecurityAlgorithms.RsaSha256Signature, SecurityAlgorithms.Sha256Digest);
+        }
+
+        public string GetEnterpriseToken()
+        {
+            string assertion = ConstructJWTAssertion(this.enterpriseId, "enterprise");
+
+            var result = JWTAuthPost(assertion);
+            return result.access_token;
+        }
+
+        public string GetUserToken(string userId)
+        {
+            string assertion = ConstructJWTAssertion(userId, "user");
+
+            var result = JWTAuthPost(assertion);
+            return result.access_token;
+        }
+
+        public string CreateAppUser(string name, string enterpriseToken)
+        {
+            var client = new RestClient(USERS_URL);
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Authorization", "Bearer " + enterpriseToken);
+            request.AddJsonBody(new { name = name, is_platform_access_only = true });
+
+            var response = client.Execute(request);
+            var content = response.Content;
+
+            dynamic parsed_content = JObject.Parse(content);
+            return parsed_content.id;
+        }
+
+        public void DeleteAppUser(string userId, string enterpriseToken, bool force = true)
+        {
+            var client = new RestClient(USERS_URL + "/" + userId);
+            var request = new RestRequest(Method.DELETE);
+            request.AddHeader("Authorization", "Bearer " + enterpriseToken);
+            request.AddParameter("force", force ? "true" : "false");
+
+            var response = client.Execute(request);
+        }
+
+        private string ConstructJWTAssertion(string sub, string boxSubType)
+        {
+            byte[] randomNumber = new byte[64];
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                rng.GetBytes(randomNumber);
+            }
+
+            var claims = new List<Claim>{
+                new Claim("sub", sub),
+                new Claim("box_sub_type", boxSubType),
+                new Claim("jti", Convert.ToBase64String(randomNumber)),
+            };
+
+            var token = new JwtSecurityToken(issuer: this.clientId, audience: AUTH_URL, claims: claims, expires: DateTime.UtcNow.AddSeconds(30),
+                            signingCredentials: this.credentials);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            string assertion = tokenHandler.WriteToken(token);
+            return assertion;
+        }
+
+        private dynamic JWTAuthPost(string assertion)
+        {
+            var client = new RestClient(AUTH_URL);
+            var request = new RestRequest(Method.POST);
+            request.AddParameter("grant_type", JWT_GRANT_TYPE);
+            request.AddParameter("client_id", this.clientId);
+            request.AddParameter("client_secret", this.clientSecret);
+            request.AddParameter("assertion", assertion);
+
+            var response = client.Execute(request);
+            var content = response.Content;
+
+            dynamic parsed_content = JObject.Parse(content);
+            return parsed_content;
+        }
+    }
+
+
+    class PEMPasswordFinder : IPasswordFinder
+    {
+        private string pword;
+
+        public PEMPasswordFinder(string password)
+        {
+            pword = password;
+        }
+
+        public char[] GetPassword()
+        {
+            return pword.ToCharArray();
+        }
+    }
+}
+
---
+++ BoxDevEditionExample/BoxDevEditionClientExample/Program.cs
@@ -0,0 +1,80 @@
+﻿using Box.V2;
+using Box.V2.Auth;
+using Box.V2.Config;
+using BoxDevEditionAuthHelper;
+using Nito.AsyncEx;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Install the follow Nuget packages:
+ * 
+ * Box Windows SDK V2
+ * 
+ * Also, add a reference to System.Configuration
+ */
+
+namespace BoxDevEditionClient
+{
+    class Program
+    {
+        static readonly string CLIENT_ID = ConfigurationManager.AppSettings["boxClientId"];
+        static readonly string CLIENT_SECRET = ConfigurationManager.AppSettings["boxClientSecret"];
+        static readonly string ENTERPRISE_ID = ConfigurationManager.AppSettings["boxEnterpriseId"];
+        static readonly string JWT_PRIVATE_KEY_PASSWORD = ConfigurationManager.AppSettings["boxPrivateKeyPassword"];
+        static readonly string JWT_PRIVATE_KEY = ConfigurationManager.AppSettings["boxPrivateKey"];
+        static readonly string REFRESH_TOKEN = "NOT_NEEDED_BUT_MUST_BE_PRESENT";
+
+        static readonly BoxJWTHelper boxJWTHelper = new BoxJWTHelper(ENTERPRISE_ID, CLIENT_ID, CLIENT_SECRET, JWT_PRIVATE_KEY, JWT_PRIVATE_KEY_PASSWORD);
+        static BoxClient adminClient;
+        static BoxClient userClient;
+
+        static void Main(string[] args)
+        {
+            //http://blog.stephencleary.com/2012/02/async-console-programs.html
+            try
+            {
+                AsyncContext.Run(() => MainAsync());
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+
+            Console.WriteLine();
+            Console.Write("Press return to exit...");
+            Console.ReadLine();
+        }
+
+
+        static async Task MainAsync()
+        {
+            var enterpriseToken = boxJWTHelper.GetEnterpriseToken();
+            var config = new BoxConfig(CLIENT_ID, CLIENT_SECRET, new Uri("http://localhost"));
+            var adminSession = new OAuthSession(enterpriseToken, REFRESH_TOKEN, 3600, "bearer");
+            adminClient = new BoxClient(config, adminSession);
+
+            string appUserId = boxJWTHelper.CreateAppUser("test user", enterpriseToken);
+            var userToken = boxJWTHelper.GetUserToken(appUserId);
+            var userSession = new OAuthSession(userToken, REFRESH_TOKEN, 3600, "bearer");
+            userClient = new BoxClient(config, userSession);
+
+            var items = await adminClient.FoldersManager.GetFolderItemsAsync("0", 100);
+            Console.WriteLine("Admin account root folder items:");
+            items.Entries.ForEach((i) => Console.WriteLine("\t{0}", i.Name));
+
+            var userDetails = await userClient.UsersManager.GetCurrentUserInformationAsync();
+            Console.WriteLine("\nApp User Details:");
+            Console.WriteLine("\tId: {0}", userDetails.Id);
+            Console.WriteLine("\tName: {0}", userDetails.Name);
+            Console.WriteLine("\tStatus: {0}", userDetails.Status);
+
+            boxJWTHelper.DeleteAppUser(appUserId, enterpriseToken);
+        }
+
+    }
+}
+
