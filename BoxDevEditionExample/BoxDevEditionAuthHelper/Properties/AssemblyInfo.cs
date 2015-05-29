6a1769087ef208af00fe62fc0ae4852a4cbb2fb4
Chad Burnette <chadburnette@me.com>
2015-05-28 23:17:47 -0400

    working on nuget package

--- BoxDevEditionExample/BoxDevEditionAuthHelper/Properties/AssemblyInfo.cs
+++ BoxDevEditionExample/BoxDevEditionAuthHelper/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BoxDevEditionAuthHelper")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helper library for Box Developer Edition")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Chad Burnette")]
 [assembly: AssemblyProduct("BoxDevEditionAuthHelper")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
